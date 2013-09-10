80126396a98d1d609692ba8d2420693e953904de
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2013-09-10 07:37:16 +0200

    file encoding and indentation fix

--- proto-oct/src/proto_oct/genfile.clj
+++ proto-oct/src/proto_oct/genfile.clj
@@ -12,24 +12,24 @@
 
 (defmacro octarine [& exprs]
   `(println
-     (poc/emit-program
-       (poc/add-expressions [] ~@exprs))))
+    (poc/emit-program
+     (poc/add-expressions [] ~@exprs))))
 
 (octarine
-  (poc/include "stdio.h" true)
-  (poc/ifdef "_WIN32"
-    [(poc/define "WINDOWS")
-     (poc/include "Windows.h" true)
-     (poc/typedef "__int8" "I8")
-     (poc/typedef "__int8" "I8")
-     (poc/typedef "__int16" "I16")
-     (poc/typedef "unsigned __int16" "U16")
-     (poc/typedef "__int32" "I32")
-     (poc/typedef "unsigned __int32" "U32")
-     (poc/typedef "__int64" "I64")
-     (poc/typedef "unsigned __int64" "U64")
-     (poc/typedef "float" "F32")
-     (poc/typedef "double" "F64")])
-    (poc/ifdef "__APPLE__"
-      [(poc/define "MACOSX")
-       ()]))
+ (poc/include "stdio.h" true)
+ (poc/ifdef "_WIN32"
+            [(poc/define "WINDOWS")
+             (poc/include "Windows.h" true)
+             (poc/typedef "__int8" "I8")
+             (poc/typedef "__int8" "I8")
+             (poc/typedef "__int16" "I16")
+             (poc/typedef "unsigned __int16" "U16")
+             (poc/typedef "__int32" "I32")
+             (poc/typedef "unsigned __int32" "U32")
+             (poc/typedef "__int64" "I64")
+             (poc/typedef "unsigned __int64" "U64")
+             (poc/typedef "float" "F32")
+             (poc/typedef "double" "F64")])
+ (poc/ifdef "__APPLE__"
+            [(poc/define "MACOSX")
+             ()]))
